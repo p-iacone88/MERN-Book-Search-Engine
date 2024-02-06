1e6bd2c4bf8ea259af9a5bcda57b8970189e33f3
[philip iacone] <[philiacone@gmail.com]>
2024-02-06 16:04:02 -0500

    started mutations.js and queries.js in client utils

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,14 @@
+// from Activity 26 in MERN
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+`;
+
+export const ADD_USER = gql`
+`;
+
+export const SAVE_BOOK = gql`
+`;
+
+export const REMOVE_BOOK = gql`
+`;
---
+++ client/src/utils/queries.js
@@ -0,0 +1,5 @@
+// from Activity 26 in MERN
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+`;
