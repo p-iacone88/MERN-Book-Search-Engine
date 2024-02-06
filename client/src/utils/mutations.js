8c8636426b33f6c993612283d250a8794fd734f9
[philip iacone] <[philiacone@gmail.com]>
2024-02-06 16:44:55 -0500

    Added Apollo db and dotenv

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,14 +1,72 @@
 // from Activity 26 in MERN
 import { gql } from '@apollo/client';
-
+// Mutation to log in a user
 export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token
+    user {
+      _id
+      username
+    }
+  }
+}
 `;
-
+// Mutation to add a new user
 export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!) {
+  addUser(username: $username, email: $email, password: $password) {
+    token
+    user {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        authors
+        bookId
+        image
+        link
+        title
+        description
+      }
+    }
+  }
+}
 `;
-
+// Mutation to save a book for a user
 export const SAVE_BOOK = gql`
+mutation saveBook($newBook: InputBook!) {
+  saveBook(newBook: $newBook) {
+    _id
+    username
+    email
+    savedBooks {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+}
 `;
-
+//Mutation to remove book from a user's saved books
 export const REMOVE_BOOK = gql`
+mutation removeBook($bookId: ID!) {
+  removeBook(bookId: $bookId) {
+    _id
+    username
+    email
+    savedBooks {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+}
 `;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,21 @@
 // from Activity 26 in MERN
 import { gql } from '@apollo/client';
-
+// GraphQL query to fetch current user's info
 export const GET_ME = gql`
+{
+  me {
+    _id
+    username
+    email
+    bookCount
+    savedBooks {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+}
 `;
