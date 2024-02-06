614d64c54e7673415c0d19a0fddba2fc4df0de04
[philip iacone] <[philiacone@gmail.com]>
2024-02-06 14:49:49 -0500

    Completed resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,14 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require('@apollo/server-express');
 
 const typeDefs = gql`
+type Book {
+  bookId: ID!
+  authors: [String]
+  description: String
+  title: String
+  image: String
+  link: String
+}
   type User {
     _id: ID!
     username: String!
@@ -13,12 +21,23 @@ const typeDefs = gql`
     token: ID!
     user: User
   }
-  type Book {
-    bookId: ID!
+  input InputBook {
+    bookId: String
     authors: [String]
-    description: String
     title: String
+    description: String
     image: String
     link: String
-  }
-`
+}
+type Query {
+    me: User
+}
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(newBook: InputBook!): User
+    removeBook(bookId: ID!): User
+}
+`;
+
+module.exports = typeDefs;
